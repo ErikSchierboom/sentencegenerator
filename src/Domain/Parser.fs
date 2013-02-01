1104869756ceb802c06d2492756ad704452b75de
Erik Schierboom <erik_schierboom@hotmail.com>
2013-02-01 19:06:37 +0100

    Started work on parser

--- src/Domain/Parser.fs
+++ src/Domain/Parser.fs
@@ -1,26 +1,20 @@
 ﻿namespace Domain
 
+open System
+
 module Parser =
+
+    let isWordSeparator x = 
+        match x with
+        | ' ' | '\t' | '\n' | ';' | ',' | '.'  | '!' | '?' -> true
+        | _  -> false
             
     let splitWords (x:string) = 
-        Seq.toList (x.Split (' ', '\t', '\n'))
-
-    let isPunctuationCharacter x = 
-        match x with
-        | ';' -> true
-        | ',' -> true
-        | '.' -> true
-        | '!' -> true
-        | '?' -> true
-        | _   -> false
-
-    let removePunctuationCharacters (x:string) = 
-        let chars = Seq.toList (x.ToCharArray())
-        let charsWithoutPunctuationCharacters = List.filter (fun c -> not (isPunctuationCharacter c)) chars
-        charsWithoutPunctuationCharacters.ToString
+        Seq.toList (x.Split (' ', '\t', '\n', ';', ',', '.', '!', '?'))
+        |> List.filter (fun w -> not (String.IsNullOrWhiteSpace(w)))
 
     let sanitizeWord (x:string) = 
-        removePunctuationCharacters x
+        x.ToLowerInvariant()
 
     let parse (x:string) =
         List.map sanitizeWord (splitWords x)
--- tests/Domain.Tests/ParserTests.fs
+++ tests/Domain.Tests/ParserTests.fs
@@ -7,10 +7,55 @@ open Xunit.Extensions
 type ParserTests() = 
 
     [<Theory>]
+    [<InlineData(' ')>]
+    [<InlineData('\t')>]
+    [<InlineData('\n')>]
     [<InlineData(';')>]
     [<InlineData(',')>]
     [<InlineData('.')>]
     [<InlineData('!')>]
     [<InlineData('?')>]
-    member this.isPunctuationCharacterReturnsTrueForPunctuationCharacters(c) =
-        Assert.True(isPunctuationCharacter c)
+    member this.isWordSeparatorOnSeparatorCharacterReturnsTrue(c) =
+        Assert.True(isWordSeparator c)
+
+    [<Theory>]
+    [<InlineData('<')>]
+    [<InlineData('>')>]
+    [<InlineData('/')>]
+    [<InlineData('\\')>]
+    [<InlineData('a')>]
+    [<InlineData('1')>]
+    member this.isWordSeparatorOnNonSeparatorCharacterReturnsFalse(c) =
+        Assert.False(isWordSeparator c)
+
+    [<Theory>]
+    [<InlineData("")>]
+    [<InlineData(" ")>]
+    [<InlineData("  ")>]
+    [<InlineData("\t")>]
+    [<InlineData("\n")>]
+    member this.splitWordsOnWhiteSpaceStringReturnsEmptyList(wordsString) =
+        let words = splitWords wordsString        
+        Assert.True(words.IsEmpty)
+
+    [<Theory>]
+    [<InlineData("hello there world")>]
+    [<InlineData("hello\nthere\nworld")>]
+    [<InlineData("hello\tthere\tworld")>]
+    [<InlineData("hello;there;world")>]
+    [<InlineData("hello,there,world")>]
+    [<InlineData("hello.there.world")>]
+    [<InlineData("hello!there!world")>]    
+    [<InlineData("hello?there?world")>]
+    member this.splitWordsReturnsListOfWords(wordsString) =
+        let words = splitWords wordsString
+        Assert.True(["hello"; "there"; "world"] = words)
+
+    [<Fact>]  
+    member this.sanitizeWordReturnsLowerCaseString() =
+        Assert.Equal<string>("hello there world", (sanitizeWord "HELLO THERE woRLD"))
+
+    [<Fact>]
+    member this.parseReturnsInputSplitIntoWordsAndSanitized() =
+        let parsedWords = parse " HELLO!THERE;   woRLD "
+        Assert.True(["hello"; "there"; "world"] = parsedWords)
