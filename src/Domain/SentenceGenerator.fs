53b0c235319f3d6fc5b37023563b7b17bbd6e0c1
Erik Schierboom <erik_schierboom@hotmail.com>
2013-05-05 16:53:50 +0200

    Stared with sentence generator

--- src/Domain/SentenceGenerator.fs
+++ src/Domain/SentenceGenerator.fs
@@ -9,4 +9,5 @@ module SentenceGenerator =
 
     let generateSentences chainSize input =
         parseWords input
+        |> List.filter (fun word -> not (wordToTextType word = Separator))
         |> createChainLinksWithProbability chainSize
